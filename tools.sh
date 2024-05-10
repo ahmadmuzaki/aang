5c2d242eb3e98e85669126cd9209e6fd0cb00b54
Ahmad Muzaki Salman Alfaris <ahmadmuzaki.s.a@gmail.com>
2024-05-10 11:11:04 +0700

    first commit

---
+++ tools.sh
@@ -0,0 +1,4 @@
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
